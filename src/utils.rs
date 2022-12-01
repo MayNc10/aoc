d47a66e1e42f5a5126f8f2a24d13618da6ef7c12
May <MayflowerC10@gmail.com>
2022-12-01 01:34:49 -0500

    Add documentation for split_by_big_gap()

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,23 @@
+/// Returns a vector of strings, split by more than one newline
+/// 
+/// # Arguments
+/// 
+/// * `s` - A string to split
+/// 
+/// # Examples
+/// 
+/// ```
+/// use aoc::utils::split_by_big_gap;
+/// assert_eq!(split_by_big_gap("1"), vec!["1"]);
+/// 
+/// assert_eq!(split_by_big_gap("1\n2\n3"), vec!["1\n2\n3"]);
+/// 
+/// assert_eq!(split_by_big_gap("1\n    \n2\n   \n3"), vec!["1", "2", "3"]);
+/// 
+/// assert_eq!(split_by_big_gap("1\n    \n2\n \n3"), vec!["1", "2", "3"]);
+/// 
+/// assert_eq!(split_by_big_gap("1\n2\n \n3"), vec!["1\n2", "3"]);
+/// ```
 pub fn split_by_big_gap(s: &str) -> Vec<&str> {
     // This is a bit slow, but it's fine for now
     let mut in_word = true;

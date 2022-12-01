2c40d8d9350529eade9a2f771a86b8ddfc053e81
May <MayflowerC10@gmail.com>
2022-12-01 00:23:57 -0500

    Add first solutions for day1

--- src/aoc2022/day1.rs
+++ src/aoc2022/day1.rs
@@ -1,7 +1,29 @@
-pub fn part1(_input: &str) {
-    panic!("{}part1 not yet implemented!", module_path!().split("::").collect::<Vec<&str>>().last().unwrap());
+pub fn part1(input: &str) {
+    let mut cals = input.split("\n    \n").collect::<Vec<&str>>();
+    let cals = cals.iter_mut().map(|s| {
+        let mut total = 0;
+        for st in s.split_ascii_whitespace() {
+            total += st.parse::<i32>().unwrap();
+        }
+        total
+    });
+    println!("{}", cals.max().unwrap());
 }
 
-pub fn part2(_input: &str) {
-    panic!("{}part2 not yet implemented!", module_path!().split("::").collect::<Vec<&str>>().last().unwrap());
+pub fn part2(input: &str) {
+    let mut cals = input.split("\n    \n").collect::<Vec<&str>>();
+    let mut cals = cals.iter_mut().map(|s| {
+        let mut total = 0;
+        for st in s.split_ascii_whitespace() {
+            total += st.parse::<i32>().unwrap();
+        }
+        total
+    }).collect::<Vec<i32>>();
+    let mut true_tot = 0;
+    for _ in 0..3 {
+        let max = cals.iter().max().unwrap();
+        true_tot += max;
+        cals.remove(cals.iter().position(|num| num == max).unwrap());
+    }
+    println!("{true_tot}");
 }

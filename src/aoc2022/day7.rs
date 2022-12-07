0a98cbb3e227a082ddda9cacc810742630f35de5
May <MayflowerC10@gmail.com>
2022-12-07 00:24:28 -0500

    Create a subroutine in day 7 for better readability

--- src/aoc2022/day7.rs
+++ src/aoc2022/day7.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-pub fn part1(input: &str) {
+fn create_fs_layout(input: &str) -> HashMap<String, usize> {
     let mut dirs = HashMap::new();
     dirs.insert(String::from("/"), 0);
     let mut current_directory = String::from("/");
@@ -36,6 +36,11 @@ pub fn part1(input: &str) {
             *current_size += size;
         }
     }
+    dirs
+}
+
+pub fn part1(input: &str) {
+    let dirs = create_fs_layout(input);
     let mut total = 0;
     for val in dirs.values() {
         if *val < 100000 {
@@ -46,41 +51,7 @@ pub fn part1(input: &str) {
 }
 
 pub fn part2(input: &str) {
-    let mut dirs = HashMap::new();
-    dirs.insert(String::from("/"), 0);
-    let mut current_directory = String::from("/");
-    for line in input.split("\n") {
-        if &line[0..4] == "$ cd" {
-            let changed_to = &line[5..line.len()];
-            if changed_to == "/" {
-                current_directory = String::from("/");
-            } 
-            else if changed_to == ".." {
-                let last_dir_idx = current_directory.rfind("^").unwrap();
-                current_directory.truncate(last_dir_idx);
-            }
-            else {
-                current_directory.push('^');
-                current_directory.push_str(&changed_to);
-            }
-        }
-        else if &line[0..1] != "$" && &line[0..3] != "dir" {
-            let mut temp_dir = current_directory.clone();
-            let size: usize = line.split_ascii_whitespace().next().unwrap().parse().unwrap();
-            while temp_dir != "/" {
-                if !dirs.contains_key(&temp_dir) {
-                    dirs.insert(temp_dir.clone(), size);
-                } else {
-                    let current_size = dirs.get_mut(&temp_dir).unwrap();
-                    *current_size += size;
-                }
-                let last_dir_idx = temp_dir.rfind("^").unwrap();
-                temp_dir.truncate(last_dir_idx);
-            }
-            let current_size = dirs.get_mut("/").unwrap();
-            *current_size += size;
-        }
-    }
+    let dirs = create_fs_layout(input);
     let total_space_remaining = 70000000 - dirs.get("/").unwrap();
     let mut vals = dirs.values().collect::<Vec<&usize>>();
     vals.sort();

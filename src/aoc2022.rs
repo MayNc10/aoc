3cbc7d333130f478a2f9b5a01548e79527993415
May <MayflowerC10@gmail.com>
2022-12-08 00:33:15 -0500

    Finish day 8

--- src/aoc2022.rs
+++ src/aoc2022.rs
@@ -35,5 +35,6 @@ pub fn main() {
     // day4::day4(fs::read_to_string("./inputs/day4.txt").unwrap().as_str());
     // day5::day5(fs::read_to_string("./inputs/day5.txt").unwrap().as_str());
     // day6::day6(fs::read_to_string("./inputs/day6.txt").unwrap().as_str());
-    day7::day7(fs::read_to_string("./inputs/day7.txt").unwrap().as_str());
+    // day7::day7(fs::read_to_string("./inputs/day7.txt").unwrap().as_str());
+    day8::day8(fs::read_to_string("./inputs/day8.txt").unwrap().as_str());
 }
--- src/aoc2022/day8.rs
+++ src/aoc2022/day8.rs
@@ -1,9 +1,133 @@
-pub fn part1(_input: &str) {
-    panic!("{}part1 not yet implemented!", module_path!().split("::").collect::<Vec<&str>>().last().unwrap());
+pub fn part1(input: &str) {
+    let mut trees = Vec::new();
+    for line in input.split("\n") {
+        let mut new_row = Vec::new();
+        for b in line.as_bytes() {
+            new_row.push(*b - 48);
+        }
+        trees.push(new_row);
+    }
+    let mut are_visible = 0;
+    for row in 1..trees.len() - 1 {
+        for col in 1..trees.len() - 1 {
+            let mut visible = true;
+            for row_idx in 0..row {
+                if trees[row_idx][col] >= trees[row][col] {
+                    visible = false;
+                }
+            }
+            if visible {
+                are_visible += 1;
+                continue;
+            }
+
+            let mut visible = true;
+            for row_idx in row + 1..trees.len() {
+                if trees[row_idx][col] >= trees[row][col] {
+                    visible = false;
+                }
+            }
+            if visible {
+                are_visible += 1;
+                continue;
+            }
+
+            let mut visible = true;
+            for col_idx in 0..col {
+                if trees[row][col_idx] >= trees[row][col] {
+                    visible = false;
+                }
+            }
+            if visible {
+                are_visible += 1;
+                continue;
+            }
+
+            let mut visible = true;
+            for col_idx in col + 1..trees.len() {
+                if trees[row][col_idx] >= trees[row][col] {
+                    visible = false;
+                }
+            }
+            if visible {
+                are_visible += 1;
+                continue;
+            }
+        }
+    }
+    are_visible += (trees.len() - 2) * 4 + 4;
+    println!("{are_visible}");
 }
 
-pub fn part2(_input: &str) {
-    panic!("{}part2 not yet implemented!", module_path!().split("::").collect::<Vec<&str>>().last().unwrap());
+pub fn part2(input: &str) {
+    let mut trees = Vec::new();
+    for line in input.split("\n") {
+        let mut new_row = Vec::new();
+        for b in line.as_bytes() {
+            new_row.push(*b - 48);
+        }
+        trees.push(new_row);
+    }
+    let mut best_scenic_score = 0;
+    for row in 1..trees.len() - 1 {
+        for col in 1..trees.len() - 1 {
+            let mut scenic_score = 1;
+ 
+            let mut broke = false;
+            for row_idx in (0..row).rev() {
+                if trees[row_idx][col] >= trees[row][col] {
+                    scenic_score *= row - row_idx;
+                    broke = true;
+                    break;
+                }
+            }
+            if !broke {
+                scenic_score *= row;
+            }
+            //println!("{scenic_score}");
+            let mut broke = false;
+            for row_idx in row + 1..trees.len() {
+                if trees[row_idx][col] >= trees[row][col] {
+                    scenic_score *= row_idx - row;
+                    broke = true;
+                    break;
+                }
+            }
+            if !broke {
+                scenic_score *= trees.len() - row - 1;
+            }
+            //println!("{scenic_score}");
+            let mut broke = false;
+            for col_idx in (0..col).rev() {
+                if trees[row][col_idx] >= trees[row][col] {
+                    scenic_score *= col - col_idx;
+                    broke = true;
+                    break;
+                }
+            }
+            if !broke {
+                scenic_score *= col;
+            }
+            //println!("{scenic_score}");
+            let mut broke = false;
+            for col_idx in col + 1..trees.len() {
+                if trees[row][col_idx] >= trees[row][col] {
+                    scenic_score *= col_idx - col;
+                    broke = true;
+                    break;
+                }
+            }
+            if !broke {
+                scenic_score *= trees.len() - col - 1;
+            }
+            //println!("{scenic_score}");
+            //println!("{} {} {}", row, col, trees[row][col]);
+            if scenic_score > best_scenic_score {
+                best_scenic_score = scenic_score;
+            }
+        }
+    }
+    println!("{best_scenic_score}");
 }
 
 pub fn day8(input: &str) {

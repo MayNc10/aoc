9a2e4ad245b7639243d939206e413a0948733a10
May <MayflowerC10@gmail.com>
2022-12-09 00:32:44 -0500

    Finish day 9

--- src/aoc2022/day9.rs
+++ src/aoc2022/day9.rs
@@ -1,9 +1,66 @@
-pub fn part1(_input: &str) {
-    panic!("{}part1 not yet implemented!", module_path!().split("::").collect::<Vec<&str>>().last().unwrap());
+use std::collections::HashSet;
+
+fn adjust(head_pos: &mut (i32, i32), tail_pos: &mut(i32, i32)) {
+    if (head_pos.0).abs_diff(tail_pos.0) < 2 && (head_pos.1).abs_diff(tail_pos.1) < 2 { return; }
+    else  {
+        if head_pos.0 > tail_pos.0 {
+            tail_pos.0 += 1;
+        } else if head_pos.0 < tail_pos.0 {
+            tail_pos.0 -= 1;
+        }
+        if head_pos.1 > tail_pos.1 {
+            tail_pos.1 += 1;
+        } else if head_pos.1 < tail_pos.1 {
+            tail_pos.1 -= 1;
+        }
+    }
 }
 
-pub fn part2(_input: &str) {
-    panic!("{}part2 not yet implemented!", module_path!().split("::").collect::<Vec<&str>>().last().unwrap());
+fn sim_movement(head_pos: &mut (i32, i32), dir: &str) {
+    match dir {
+        "R" => head_pos.0 += 1,
+        "L" => head_pos.0 -= 1,
+        "U" => head_pos.1 += 1,
+        "D" => head_pos.1 -= 1,
+        _ => panic!("Had dir {}", dir),
+    }
+}
+
+pub fn part1(input: &str) {
+    let mut head_pos = (0,0);
+    let mut tail_pos = (0,0);
+    let mut positions = HashSet::new();
+    positions.insert(tail_pos);
+    for line in input.split("\n") {
+        let dir = &line[0..1];
+        let amount = line[2..].parse().unwrap();
+        for _ in 0..amount {
+            sim_movement(&mut head_pos, dir);
+            adjust(&mut head_pos, &mut tail_pos);
+            positions.insert(tail_pos);
+        }
+    }
+    
+    println!("{}", positions.len());
+}
+
+pub fn part2(input: &str) {
+    let mut knots = [(0,0); 10];
+    let mut positions = HashSet::new();
+    positions.insert(knots[9]);
+    for line in input.split("\n") {
+        let dir = &line[0..1];
+        let amount = line[2..].parse().unwrap();
+        for _ in 0..amount {
+            sim_movement(&mut knots[0], dir);
+            for idx in 0..9 {
+                let (before, after) = knots.split_at_mut(idx + 1);
+                adjust(&mut before[idx], &mut after[0]);
+            }
+            positions.insert(knots[9]);
+        }
+    }
+    println!("{}", positions.len());
 }
 
 pub fn day9(input: &str) {

a19d1b62c2d763d7f918e0309a2bdb4b99ddcdde
May <MejeC10@gmail.com>
2022-12-10 01:28:19 -0500

    Improve readability of day 10

--- src/aoc2022/day10.rs
+++ src/aoc2022/day10.rs
@@ -1,67 +1,56 @@
-pub fn part1(input: &str) {
+fn loop_lines<F>(input: &str, mut f: F)
+where
+    F: FnMut(i32, i32) -> bool,
+{
     let mut x = 1;
     let mut cycle = 1;
-    let mut sum = 0;
     for line in input.split("\n") {
-        if cycle > 220 {
+        if f(cycle, x) {
             break;
         }
-        else if (cycle - 20) % 40 == 0 {
-            //println!("Cycle {}, adding {}", cycle, cycle * x);
-            sum += cycle * x;
-        } 
         if line == "noop" {
             cycle += 1;
-        } else {
+        }
+        else {
             let add: i32 = line.split_ascii_whitespace().skip(1).next().unwrap().parse().unwrap();
             cycle += 1;
-            if cycle > 220 {
+            if f(cycle, x) {
                 break;
             }
-            else if (cycle - 20) % 40 == 0 {
-                sum += cycle * x;
-            } 
-            //println!("{line}, Adding {add}");
             x += add;
             cycle += 1;
         }
     }
+}
+
+pub fn part1(input: &str) {
+    let mut sum = 0;
+    loop_lines(input, |cycle, x| {
+        if cycle > 220 {
+            return true;
+        }
+        else if (cycle - 20) % 40 == 0 {
+            sum += cycle * x;
+        } 
+        false
+    });
     println!("{sum}");
 }
 
 pub fn part2(input: &str) {
-    let mut x = 1;
-    let mut pos = 0;
-    for line in input.split("\n") {
+    loop_lines(input, |cycle, x| {
+        let pos = cycle - 1;
         if (pos % 40 as i32).abs_diff(x) < 2 {
             print!("#");
         }
         else {
             print!(".");
         }
-        if (pos + 1) % 40 == 0 {
+        if cycle % 40 == 0 {
             println!();
         }
-        if line == "noop" {
-            pos += 1;
-        } else {
-            let add: i32 = line.split_ascii_whitespace().skip(1).next().unwrap().parse().unwrap();
-            pos += 1;
-            if (pos % 40 as i32).abs_diff(x) < 2 {
-                print!("#");
-            }
-            else {
-                print!(".");
-            }
-            if (pos + 1) % 40 == 0 {
-                println!();
-            }
-            //println!("{line}, Adding {add}");
-            x += add;
-            pos += 1;
-        }
-    }
-    
+        false
+    });    
 }
 
 pub fn day10(input: &str) {
